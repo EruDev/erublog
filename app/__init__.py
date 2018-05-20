bd3a4a8f7d4357576fee2f06ed4185da2637f6d5
EruDev <1027926875@qq.com>
2018-05-20 16:32:34 +0800

    heroku deploy changes

--- app/__init__.py
+++ app/__init__.py
@@ -64,7 +64,7 @@ def create_app(config_class=Config):
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
-        if app.config['LOG_TOSTDOUT']:
+        if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)

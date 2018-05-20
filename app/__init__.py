72a0eb3e9dbd6efa1184940e4365ad3128b51a6e
EruDev <1027926875@qq.com>
2018-05-20 15:03:40 +0800

    3

--- app/__init__.py
+++ app/__init__.py
@@ -42,7 +42,7 @@ def create_app(config_class=Config):
     app.register_blueprint(errors_bp)
 
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)

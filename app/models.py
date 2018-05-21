28d83fddbd232ce3f4295496af9059940c93e61c
EruDev <1027926875@qq.com>
2018-05-21 08:13:56 +0800

    4

--- app/models.py
+++ app/models.py
@@ -1,9 +1,7 @@
-import base64
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
 from time import time
 import jwt
-import os
 from flask import current_app, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -47,25 +45,25 @@ class SearchableMixin(object):
             add_to_index(cls.__tablename__, obj)
 
 
-class PaginatedAPIMixin(object):
-    @staticmethod
-    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
-        resources = query.paginate(page, per_page, False)
-        data = {
-            'items': [item.to_dict() for item in resources.items],
-            '_meta': {
-                'page': page,
-                'per_page': per_page,
-                'total_pages': resources.pages,
-                'total_items': resources.total
-            },
-            '_links':{
-                'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
-                'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if resources.has_next else None,
-                'prev': url_for(endpoint, page = page - 1, per_page=per_page, **kwargs) if resources.has_prev else None
-            }
-        }
-        return data
+# class PaginatedAPIMixin(object):
+#     @staticmethod
+#     def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+#         resources = query.paginate(page, per_page, False)
+#         data = {
+#             'items': [item.to_dict() for item in resources.items],
+#             '_meta': {
+#                 'page': page,
+#                 'per_page': per_page,
+#                 'total_pages': resources.pages,
+#                 'total_items': resources.total
+#             },
+#             '_links':{
+#                 'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+#                 'next': url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if resources.has_next else None,
+#                 'prev': url_for(endpoint, page = page - 1, per_page=per_page, **kwargs) if resources.has_prev else None
+#             }
+#         }
+#         return data
 
 
 
@@ -75,7 +73,7 @@ followers = db.Table('followers',
  )
 
 
-class User(PaginatedAPIMixin, UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -155,52 +153,52 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
         db.session.add(n)
         return n
 
-    def to_dict(self, include_email=False):
-        data = {
-            'id': self.id,
-            'username': self.username,
-            'last_seen': self.last_seen.isoformat() + 'Z',
-            'about_me': self.about_me,
-            'post_count': self.posts.count(),
-            'follower_count': self.followers.count(),
-            'followed_count': self.followed.count(),
-            '_links': {
-                'self': url_for('api.get_user', id=self.id),
-                'followers': url_for('api.get_followers', id=self.id),
-                'followed': url_for('api.get_followed', id=self.id),
-                'avatar': self.avatar(128)
-            }
-        }
-        if include_email:
-            data['email'] = self.email
-        return data
-
-    def from_dict(self, data, new_user=False):
-        for field in ['username', 'email', 'about_me']:
-            if field in data:
-                setattr(self, field, data[field])
-
-        if new_user and 'password' in data:
-            self.set_password(data['password'])
-
-    def get_token(self, expires_in=3600):
-        now = datetime.utcnow()
-        if self.token and self.token_expiration > now + timedelta(seconds=60):
-            return self.token
-        self.token = base64.b64decode(os.urandom(24)).decode('utf-8')
-        self.token_expiration = now + timedelta(seconds=expires_in)
-        db.session.add(self)
-        return self.token
-
-    def revoke_token(self):
-        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
-
-    @staticmethod
-    def check_token(token):
-        user = User.query.filter_by(token=token).first()
-        if user is None or user.token_expiration < datetime.utcnow():
-            return None
-        return user
+    # def to_dict(self, include_email=False):
+    #     data = {
+    #         'id': self.id,
+    #         'username': self.username,
+    #         'last_seen': self.last_seen.isoformat() + 'Z',
+    #         'about_me': self.about_me,
+    #         'post_count': self.posts.count(),
+    #         'follower_count': self.followers.count(),
+    #         'followed_count': self.followed.count(),
+    #         '_links': {
+    #             'self': url_for('api.get_user', id=self.id),
+    #             'followers': url_for('api.get_followers', id=self.id),
+    #             'followed': url_for('api.get_followed', id=self.id),
+    #             'avatar': self.avatar(128)
+    #         }
+    #     }
+    #     if include_email:
+    #         data['email'] = self.email
+    #     return data
+    #
+    # def from_dict(self, data, new_user=False):
+    #     for field in ['username', 'email', 'about_me']:
+    #         if field in data:
+    #             setattr(self, field, data[field])
+    #
+    #     if new_user and 'password' in data:
+    #         self.set_password(data['password'])
+    #
+    # def get_token(self, expires_in=3600):
+    #     now = datetime.utcnow()
+    #     if self.token and self.token_expiration > now + timedelta(seconds=60):
+    #         return self.token
+    #     self.token = base64.b64decode(os.urandom(24)).decode('utf-8')
+    #     self.token_expiration = now + timedelta(seconds=expires_in)
+    #     db.session.add(self)
+    #     return self.token
+    #
+    # def revoke_token(self):
+    #     self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+    #
+    # @staticmethod
+    # def check_token(token):
+    #     user = User.query.filter_by(token=token).first()
+    #     if user is None or user.token_expiration < datetime.utcnow():
+    #         return None
+    #     return user
 
 
 class Post(SearchableMixin, db.Model):
